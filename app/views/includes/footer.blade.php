b9c7d100264b28ec92a7cca1acdb9c5e6313523b
Edi Santoso <edicyber@gmail.com>
2014-02-16 15:59:13 +0700

    add local asset css and js

--- app/views/includes/footer.blade.php
+++ app/views/includes/footer.blade.php
@@ -1,1 +1,5 @@
 <div id="copyright text-right">Copyright &copy; 2014 - By edicyber@gmail.com</div>
+
+ <!-- Scripts are placed here -->
+{{ HTML::script('public/js/jquery.min.js') }}
+{{ HTML::script('public/js/bootstrap.min.js') }}
--- app/views/includes/head.blade.php
+++ app/views/includes/head.blade.php
@@ -3,14 +3,5 @@
 <meta name="author" content="Scotch">
 
 <title>Company Profile - Laravel</title>
-
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-
-<script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+{{ HTML::style('public/css/bootstrap.css') }}
+{{ HTML::style('public/css/bootstrap.min.css') }}
