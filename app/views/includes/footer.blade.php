66aa97b0957e02103c894c5bdf45b55147680a6c
Edi Santoso <edicyber@gmail.com>
2014-02-16 20:05:13 +0700

    adding sticky footer

--- app/views/includes/footer.blade.php
+++ app/views/includes/footer.blade.php
@@ -1,4 +1,8 @@
-<div id="copyright text-right">Copyright &copy; 2014 - By edicyber@gmail.com</div>
+<div id="footer">
+  <div class="container">
+    <p class="text-muted credit">Example courtesy <a href="http://martinbean.co.uk/">Martin Bean</a> and <a href="http://ryanfait.com/sticky-footer/">Ryan Fait</a>.</p>
+  </div>
+</div>
 
  <!-- Scripts are placed here -->
 {{ HTML::script('public/js/jquery.min.js') }}
--- app/views/includes/head.blade.php
+++ app/views/includes/head.blade.php
@@ -5,3 +5,4 @@
 <title>Company Profile - Laravel</title>
 {{ HTML::style('public/css/bootstrap.css') }}
 {{ HTML::style('public/css/bootstrap.min.css') }}
+{{ HTML::style('public/css/sticky-footer.css') }}
