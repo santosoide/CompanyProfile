1b9bdd729067e492c158e7d6881852086ea8d26b
Edi Santoso <edicyber@gmail.com>
2014-02-16 18:13:55 +0700

    fixed bugs

--- app/views/includes/footer.blade.php
+++ app/views/includes/footer.blade.php
@@ -1,5 +1,5 @@
 <div id="copyright text-right">Copyright &copy; 2014 - By edicyber@gmail.com</div>
 
  <!-- Scripts are placed here -->
-{{ HTML::script('js/jquery.min.js') }}
-{{ HTML::script('js/bootstrap.min.js') }}
+{{ HTML::script('public/js/jquery.min.js') }}
+{{ HTML::script('public/js/bootstrap.min.js') }}
