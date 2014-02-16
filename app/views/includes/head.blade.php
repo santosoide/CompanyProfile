35e18074633001784ff9fecf37fd2b5d59809caf
Edi Santoso <edicyber@gmail.com>
2014-02-16 16:53:50 +0700

    setting up new root
    moving index.php to /public/index.php
    fixed bugs for assets without call directory public

--- app/views/includes/head.blade.php
+++ app/views/includes/head.blade.php
@@ -3,5 +3,5 @@
 <meta name="author" content="Scotch">
 
 <title>Company Profile - Laravel</title>
-{{ HTML::style('public/css/bootstrap.css') }}
-{{ HTML::style('public/css/bootstrap.min.css') }}
+{{ HTML::style('css/bootstrap.css') }}
+{{ HTML::style('css/bootstrap.min.css') }}
